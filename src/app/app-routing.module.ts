805d6c7a4b568a9c8e9f36de12d89b83295b5482
Alex J.Y <13817282645@163.com>
2018-03-10 15:54:10 +0800

    1. Wildcard Route

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,43 @@
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { ComposeMessageComponent }  from './compose-message.component';
+import { PageNotFoundComponent }    from './not-found.component';
+
+import { CanDeactivateGuard }       from './can-deactivate-guard.service';
+import { AuthGuard }                from './auth-guard.service';
+import { SelectivePreloadingStrategy } from './selective-preloading-strategy';
+import { HeroListComponent } from "./heroes/hero-list.component";
+
+const appRoutes: Routes = [
+
+  {
+    path: 'crisis-center',
+    loadChildren: 'app/crisis-center/crisis-center.module#CrisisCenterModule',
+    data: { preload: true }
+  },
+  { path: 'heroes', component: HeroListComponent },
+  // { path: '',   redirectTo: '/superheroes', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      {
+        enableTracing: true, // <-- debugging purposes only
+        preloadingStrategy: SelectivePreloadingStrategy,
+
+      }
+    )
+  ],
+  exports: [
+    RouterModule
+  ],
+  providers: [
+    CanDeactivateGuard,
+    SelectivePreloadingStrategy
+  ]
+})
+export class AppRoutingModule { }
